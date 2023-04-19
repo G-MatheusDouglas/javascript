2c4414a6790dbbf1c0e8fe340a9792bf5fb4a9c5
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-04-18 22:18:22 -0300

    Create ex016
    While and Do

---
+++ exercicios/ex016/script.js
@@ -0,0 +1,20 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 8)
+
+/* while (c <= 8) {
+console.log(`Passo ${c}`)
+    c++
+} */
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
