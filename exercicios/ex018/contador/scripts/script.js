1b965c026fe4caa1a73778e54b1ad4146e65336b
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-04-26 21:18:55 -0300

    Contador 1.3

--- exercicios/ex018/contador/scripts/script.js
+++ exercicios/ex018/contador/scripts/script.js
@@ -1,19 +1,30 @@
-function verificar() {
-    var numIni = Number(document.querySelector('input#numIni').value)
-    var numFim = Number(document.querySelector('input#numFim').value)
-    var passo = Number(document.querySelector('input#passo').value)
-    var res = document.querySelector('div#res>p')
-    res.innerHTML = 'Contando:<br>'
-    if (numIni == '' || numFim == ''){
-        res.innerHTML = 'Impossivel contar!'
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.querySelector('div#res>p')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar'
     } else {
-        if (passo == 0){
-            window.alert('Passo inválido Considerando PASSO 1')
-            passo = 1
+        res.innerHTML = 'Contando:<br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p == 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-        while (numIni <= numFim) {
-            res.innerHTML += `${numIni} &#x1F449`
-            numIni += passo
+        if (i < f) {
+            // Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} &#x1F449`
+            }
+        } else {
+            // Contagem Decrescente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} &#x1F449`
+            }
         }
         res.innerHTML += '&#x1F3C1'
     }
